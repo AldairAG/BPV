93aa868d8023a91702b721da35fdb25046893fe1
AldairAG <alagga02@gmail.com>
2025-06-01 21:45:35 -0600

    feat: Update frontend configuration with environment variables and enhance API client setup

--- frontend/src/service/apiClient.ts
+++ frontend/src/service/apiClient.ts
@@ -1,6 +1,6 @@
 // apiClient.js
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8080/lbf'; // ajusta la URL
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/lbf';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
